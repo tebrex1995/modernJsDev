9ff99acaf970dcfe593f5cdc98726cc810e51843
tebrex1995 <tvrdisica@gmail.com>
2024-11-19 12:50:33 +0100

    added loadash

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 // Importing module
 
-// import {
-//   addToCart,
-//   totalPrice as price,
-//   totalQuantity as tq,
-// } from './shoppingCart.js';
-// ('./shoppingCart.js');
+//** */
+// // import {
+// //   addToCart,
+// //   totalPrice as price,
+// //   totalQuantity as tq,
+// // } from './shoppingCart.js';
+// // ('./shoppingCart.js');
 
-// import * as ShoppingCart from './shoppingCart.js';
+// // import * as ShoppingCart from './shoppingCart.js';
 // import add, { cart } from './shoppingCart.js';
 
 // console.log('Importing module');
@@ -67,3 +68,32 @@
 // ShoppingCart2.addToCart('pizza', 2);
 
 // console.log(ShoppingCart2);
+
+// //Export
+// export.addToCart = function (product, quantity) {
+//   cart.push({ product, quantity });
+//   console.log(
+//     `${quantity} ${product} was added to cart! ( Shipping cost is ${shippingCost} )`
+//   );
+// };
+
+// //Import
+// const {addToCart} = require('./shoppingCart.js')
+
+import cloneDeep from '/node_modules/lodash-es/cloneDeep.js';
+
+const state = {
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 5 },
+  ],
+  user: { loggedIn: true },
+};
+
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+
+state.user.loggedIn = false;
+console.log(stateClone);
+
+console.log(stateDeepClone);
